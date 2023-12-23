3c06068497d2233326fab53ee4b17117f464e41d
Pavel <kamper.inc@yandex.ru>
2023-12-23 08:02:25 +0400

    fix: datetime errors

--- stats-service/server/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ stats-service/server/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,33 +1,41 @@
 package ru.practicum.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @Slf4j
 @RestControllerAdvice
+@ResponseBody
 public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleBadRequestException(final BadRequestException e) {
-        log.info("400 {}", e.getMessage());
-        return new ErrorResponse(
-                "BAD_REQUEST",
-                "Invalid request parameter",
-                e.getMessage());
+    public ApiError handleInvalidDatesException(BadRequestException e) {
+        return new ApiError("FORBIDDEN", "For the requested operation the conditions are not met.",
+                e.getMessage(), LocalDateTime.now());
     }
 
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleRuntimeException(final Throwable e) {
-        log.warn("500 {}", e.getMessage(), e);
-        return new ErrorResponse(
-                "RuntimeException",
-                "INTERNAL_SERVER_ERROR",
-                e.getMessage()
-        );
+    @Getter
+    static class ApiError {
+        private final String status;
+        private final String reason;
+        private final String message;
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private final LocalDateTime timestamp;
+
+        public ApiError(String status, String reason, String message, LocalDateTime timestamp) {
+            this.status = status;
+            this.reason = reason;
+            this.message = message;
+            this.timestamp = timestamp;
+        }
     }
 }
--- stats-service/server/src/main/java/ru/practicum/exception/ErrorResponse.java
+++ stats-service/server/src/main/java/ru/practicum/exception/ErrorResponse.java
@@ -9,12 +9,12 @@ public class ErrorResponse {
     private String status;
     private String reason;
     private String message;
-    private String timestamp;
+    private LocalDateTime timestamp;
 
     public ErrorResponse(String status, String reason, String message) {
         this.status = status;
         this.reason = reason;
         this.message = message;
-        this.timestamp = DateTimeMapper.toStringDate(LocalDateTime.now());
+        this.timestamp = LocalDateTime.now();
     }
 }
