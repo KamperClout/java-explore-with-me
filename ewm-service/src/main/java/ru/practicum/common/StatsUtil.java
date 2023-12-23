dbc8cd9a486bdf2f1767dc6ced3803ace0326abf
Pavel <kamper.inc@yandex.ru>
2023-12-23 04:25:59 +0400

    fix: optimize imports

--- ewm-service/src/main/java/ru/practicum/common/StatsUtil.java
+++ ewm-service/src/main/java/ru/practicum/common/StatsUtil.java
@@ -2,7 +2,6 @@ package ru.practicum.common;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.practicum.StatsClient;
 import ru.practicum.dto.EndpointHitDto;
